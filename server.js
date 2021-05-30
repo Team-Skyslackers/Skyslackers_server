0e17d3b33517136fc473effcbc4e559bae5708b9
Yue Junfeng <yuejunfeng0909@gmail.com>
2021-05-31 07:45:40 +0800

    Able to send all gyroschope data

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-/*const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
-
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8000);
-
-
-var express = require('express')
-var ws = require('ws')
-var app = express()
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-})
-app.listen(3000, function () {
-   console.log('Example app listening on port 3000!')
-})  
-*/
 const https = require('https');
 const fs = require('fs');
 var express = require('express')
@@ -29,29 +5,44 @@ var app = express()
 // readFileSync function must use __dirname get current directory
 // require use ./ refer to current directory.
 
-const options = {
+const certs = {
    key: fs.readFileSync(__dirname + '/192.168.3.48-key.pem', 'utf8'),
   cert: fs.readFileSync(__dirname + '/192.168.3.48.pem', 'utf8')
 };
 
 
  // Create HTTPs server.
-
- var httpsServer = https.createServer(options, app).listen(8000, function () {
+ var httpsServer = https.createServer(certs, app).listen(8000, function () {
   console.log("server running at https://IP_ADDRESS:8000/")
 });
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
+var rd = "0 0 0";
 var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({server: httpsServer})
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
-    console.log('received: %s', message)
+    console.log('received: %s', message);
+    rd = message;
   })
   setInterval(
     () => ws.send(`${new Date()}`),
     1000
+  )
+})
+
+const ws_Unity = require('ws')
+const wss_Unity = new ws_Unity.Server({ port: 8080 },()=>{
+    console.log('server started')
+})
+wss_Unity.on('listening',()=>{
+   console.log('Unity server listening on 8080')
+})
+wss_Unity.on('connection', function connection(ws_Unity) {
+  setInterval(
+    () => {ws_Unity.send(rd); console.log('data sent to Unity: %s', rd);},
+    17
   )
 })
